fd33a0ded74529df8759f836c579967863031d56
caleb <calebh210@github.com>
2023-01-24 09:52:59 -0500

    added comments and re-made file

--- server/app/api/adminLogin.js
+++ server/app/api/adminLogin.js
@@ -1,10 +1,11 @@
+//Test page for admin authentication. To be removed and replaced with admin panel
 const express = require("express");
 
 const router = express.Router();
-router.post("/", (req, res) => {
+router.get("/", (req, res) => {
   res.status(200).send({
     success: true,
-    data: "working",
+    message: "working",
   });
 });
 
