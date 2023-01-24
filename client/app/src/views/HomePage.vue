c5135f2e64d4dc9775ad6837bbc2f492e724b05e
Jaibeer  S Dugal <jaibeer72@gmail.com>
2023-01-24 19:39:20 +0000

    commiting some env changes

---
+++ client/app/src/views/HomePage.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Header from "./components/Header.vue"
+import PictureCarosil from "./components/PictureCarosil.vue";
+
+</script>
+
+<template lang="">
+    <div>
+        <Header />
+        <PictureCarosil />
+    </div>
+</template>
+<style lang="">
+</style>
