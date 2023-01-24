5b647ea05a8f6d0ef148f692ad6d9dbe8d34a83c
David Topping <2412905@dundee.ac.uk>
2023-01-24 17:39:02 +0000

    removed all stock vue and added in logo

---
+++ client/app/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template lang="">
+    <div>
+        <img class="logo" src="@/assets/logo.webp" alt="" width="258" height="172">
+    </div>
+</template>
+
+
+<script>
+export default {
+
+}
+
+</script>
+<style >
+.logo {
+    align-items: center;
+}
+</style>
