f2d684b0c5fd5d3a8021e832b4a175a26d7995c0
Caleb <caleb.harmon2345@gmail.com>
2023-01-25 22:07:02 +0000

    Revert "Merge branch 'browseUpdated' into browse"
    This reverts commit 5d068ee8aefc62ba8210b270b4a23abffb08864e, reversing
    changes made to b0842299a342d140d88652c6db680eebefd0c66b.

--- client/app/src/router/index.js
+++ client/app/src/router/index.js
@@ -1,8 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomePage from "../views/HomePage.vue"
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
-
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
@@ -19,7 +18,6 @@ const router = createRouter({
       component: () => import("../views/BrowseView.vue"),
     },
   ],
-
 });
 
 export default router;
