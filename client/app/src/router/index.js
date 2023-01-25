bc1eb3f3a26e9c23f9b7b1fed3efa07fbcf0d838
caleb <calebh210@github.com>
2023-01-25 09:01:06 -0500

    Added BrowseView dropdown menu

--- client/app/src/router/index.js
+++ client/app/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "../views/HomePage.vue"
 
 const router = createRouter({
+
     history: createWebHistory(),
     routes: [
         {
@@ -16,7 +17,15 @@ const router = createRouter({
         //         // this generates a separate chunk (About.[hash].js) for this route
         //         // which is lazy-loaded when the route is visited.
         //     },
-    ],
+    {
+      path: "/browse",
+      name: "browse",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/BrowseView.vue"),
+    },
+  ],
 });
 
 export default router;
---
+++ client/app/src/views/BrowseView.vue
@@ -0,0 +1,59 @@
+<!-- View created for the browse view -->
+
+<template>
+  <div class="browse">
+    <h1>Browse Store</h1>
+  </div>
+
+ <v-btn block @click="toggle"> Browse by Category </v-btn>
+  <div v-if="active">
+    <tr>
+      <button @click="search(1)"> Organic </button>
+    </tr>
+    <tr>
+      <button @click="search(2)"> Wheat </button>
+    </tr>
+    <tr>
+      <button @click="search(3)"> Banana </button>
+    </tr>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        active: false
+      }
+    },
+    methods: {
+      toggle () {
+        this.active = !this.active
+      },
+
+      search (category) {
+        this.active = !this.active
+        console.log(category);
+      }
+    }
+  }
+</script>
+
+
+<style scoped>
+@media (min-width: 1024px) {
+  .browse {
+    min-height: 100vh;
+    display: flex;
+    place-items: flex-start;
+    left: 50%;
+    flex-wrap: wrap;
+    
+    
+  }
+}
+
+
+
+</style>
