6d7c39c0f84bb49565c8eb18e7dc8a2302a1b791
Caleb <caleb.harmon2345@gmail.com>
2023-01-25 22:07:02 +0000

    Revert "Merge branch 'browseUpdated' into browse"
    This reverts commit 5d068ee8aefc62ba8210b270b4a23abffb08864e, reversing
    changes made to b0842299a342d140d88652c6db680eebefd0c66b.

--- client/app/src/main.js
+++ client/app/src/main.js
@@ -6,12 +6,11 @@ import "bootstrap"
 import App from "./App.vue";
 import router from "./router";
 
-const app = createApp(App);
+import "./assets/main.css";
 
+const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 
 app.mount("#app");
-
-import "bootstrap/dist/js/bootstrap.js"; 
