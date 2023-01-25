5fec3c806ba6871bb78665c1dde91239276cc116
Jaibeer  S Dugal <jaibeer72@gmail.com>
2023-01-25 16:00:57 +0000

    added new app with bootstrap support

---
+++ client/app/src/views/HomePage.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+
+        <div class="row">
+            <div class="col">
+                <img src="../assets/logo.webp" alt="">
+            </div>
+        </div>
+
+
+        <div class="row">
+            <div class="col mt-2">
+                <IngredientSearch />
+            </div>
+        </div>
+
+        <div>
+
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+import IngredientSearch from "../components/IngredientSearch.vue";
+</script>
+
+<style >
+img {
+    height: 120px;
+}
+</style>
