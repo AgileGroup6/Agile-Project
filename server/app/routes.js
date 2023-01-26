c8e59f896cef33aba73c9b11f904accc3c42d428
caydey <caydey@protonmail.com>
2023-01-26 10:34:30 +0000

    added allRecipes endpoint

--- server/app/routes.js
+++ server/app/routes.js
@@ -1,17 +1,18 @@
 const allIngredients = require("./api/allIngredients.js");
 const adminLogin = require("./api/adminLogin.js");
 
+const allRecipes = require('./api/allRecipes.js');
+
 const findRecipe = require("./api/findRecipe.js");
-const getRecipe = require("./api/getRecipe.js");
 
 const accessController = require("./middleware/accessController.js");
 
 const routes = (route) => {
   route.use("/api/allIngredients", allIngredients.router);
 
-  route.use("/api/findRecipe", findRecipe.router);
+  route.use("/api/allRecipes", allRecipes.router)
 
-  route.use("/api/getRecipe", getRecipe.router);
+  route.use("/api/findRecipe", findRecipe.router);
 
   route.use(
     "/api/adminLogin",
