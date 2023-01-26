b97b574ac91d9d057b0599ad5a74312ae6fdf293
FinnHagan <2410686@dundee.ac.uk>
2023-01-26 16:12:19 +0000

    Bootstrap styling for admin login

--- client/app/src/App.vue
+++ client/app/src/App.vue
@@ -1,25 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
-
 </script>
 
 <template>
- <RouterView />
-  <header>
-    
-
-    <nav>
-      <img id="homeRoute" src="./assets/home.png" alt="">
-    </nav>
-  </header>
-
   <RouterView />
 </template>
 
 <style>
-#homeRoute{
-  
+#homeRoute {
+
   height: 10vw;
 }
 </style>
