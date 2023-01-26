28c0a1e523653d3c34e5a83041b19cc195717084
David Topping <2412905@dundee.ac.uk>
2023-01-26 12:57:46 +0000

    added home page with logo,list and browse all button

---
+++ client/app/src/views/HomePage.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container">
+
+      <div class="row">
+          <div class="col">
+              <img src="../assets/logo.webp" alt="">
+          </div>
+      </div>
+
+
+      <div class="row">
+          <div class="col mt-2">
+              <IngredientSearch />
+              <ListItem :ingredients="shoppingList"/>
+          </div>
+      </div>
+
+      <ButtonComponent  label="Browse all" :doOnClick="testFunction"/>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+
+  data() {
+        return {
+            shoppingList : [{ ing: 'Curry Powder' }, { ing: 'Pepper' }],
+        }
+    
+},
+
+methods: {
+  testFunction: function () {
+    console.log('test clicked')
+  }
+}
+}
+</script>
+
+<script setup>
+import IngredientSearch from "../components/homePage/IngredientSearch.vue";
+import ButtonComponent from "@/components/homePage/ButtonComponent.vue";
+import ListItem from "../components/homePage/ListItem.vue";
+</script>
+
+<style >
+img {
+  height: 120px;
+}
+</style>
