3e934270f321471abcc0c083240fac2c3a0f8e8b
caydey <caydey@protonmail.com>
2023-01-26 20:11:24 +0000

    Updated admin api checker

--- server/app/routes.js
+++ server/app/routes.js
@@ -1,23 +1,27 @@
-const allIngredients = require("./api/allIngredients.js");
-const adminLogin = require("./api/adminLogin.js");
+const allIngredients = require("./api/allIngredients");
+const allRecipes = require("./api/allRecipes");
+const findRecipe = require("./api/findRecipe");
 
-const allRecipes = require('./api/allRecipes.js');
+const addRecipe = require("./api/addRecipe");
 
-const findRecipe = require("./api/findRecipe.js");
+const checkAdminPassword = require("./api/checkAdminPassword");
 
-const accessController = require("./middleware/accessController.js");
+const accessController = require("./middleware/accessController");
 
 const routes = (route) => {
   route.use("/api/allIngredients", allIngredients.router);
 
-  route.use("/api/allRecipes", allRecipes.router)
+  route.use("/api/allRecipes", allRecipes.router);
 
   route.use("/api/findRecipe", findRecipe.router);
 
+  route.use("/api/checkAdminPassword", checkAdminPassword.router);
+
+  // not used at the moment, testing admin authentication
   route.use(
-    "/api/adminLogin",
-    accessController, //Requires authorization to access admin panel
-    adminLogin.router
+    "/api/addRecipe",
+    accessController, // must have admin cookie set for this
+    addRecipe.router
   );
 };
 
