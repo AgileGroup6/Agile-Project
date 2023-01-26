4aa33f460bb8dcfac11eea733f988678b01dd070
benduguid <ben.duguid8@gmail.com>
2023-01-26 16:42:46 +0000

    Add Recipe page ready for client
    Co-Authored-By: caydey <63204672+caydey@users.noreply.github.com>

--- client/app/src/main.js
+++ client/app/src/main.js
@@ -6,7 +6,6 @@ import "bootstrap"
 import App from "./App.vue";
 import router from "./router";
 
-import "./assets/main.css";
 import './registerServiceWorker'
 
 const app = createApp(App);
--- client/app/src/views/AddRecipe.vue
+++ client/app/src/views/AddRecipe.vue
@@ -3,242 +3,186 @@
     <head>
       <title>Add Recipe</title>
     </head>
-
-    <header>
-      <h1 class="title">Admin Page</h1>
-    </header>
-
-    <form>
-      <div class="input">
-        <label class="left-text" for="title">Title *</label>
-        <input
+    <!-- <form class="container"><header><h1 class="title">Add Recipe</h1></header><div class="row"><label class="left-text" for="title">Title *</label><input
           class="bar"
           type="text"
           name="title"
           placeholder="Title"
           v-model="title"
           required
-        />
-      </div>
-
-      <div class="input">
-        <label for="serves">Serves</label>
-        <input
+        /></div><div class="row"><label class="left-text" for="serves">Serves</label><input
           type="number"
           name="serves"
           class="bar"
           placeholder="Serves"
           v-model="serves"
           required
-        />
-      </div>
-
-      <div class="input">
-        <label class="left-text" for="cars">Choose your ingredients:</label>
-        <select class="bar" name="ingredients" v-model="ingredient">
-          <option v-for="ingredient in ingredients" :key="ingredient.id">
+        /></div><div class="row"><label class="left-text" for="cars">Choose your ingredients:</label><select class="bar" name="ingredients" v-model="ingredient"><option v-for="ingredient in ingredients" :key="ingredient.id">
             {{ ingredient.name }}
-          </option>
-        </select>
-        <input
+          </option></select><input
           class="bar"
           type="text"
           name="amount"
           placeholder="Amount"
           v-model="amount"
           required
-        />
-        <button type="button" @click="addMeasurement">Add</button>
-      </div>
-
-      <div ref="selectedMeasurements">
-        <h2></h2>
-        <div>
-          <div v-for="measurement in selectedMeasurements" :key="measurement">
+        /><button type="button" @click="addMeasurement">Add</button></div><div class="row" ref="selectedMeasurements"><h2></h2><div><div class="left-text" v-for="measurement in selectedMeasurements" :key="measurement">
             {{ measurement.ingredient }}
             {{ measurement.amount }}
-            <button @click="deleteMeasurements(measurement)">Delete</button>
-          </div>
-        </div>
-      </div>
-
-      <div class="input">
-        <label class="left-text" for="instructions">Instructions *</label>
-        <textarea
+            <button @click="deleteMeasurements(measurement)">Delete</button></div></div></div><div class="row"><label class="left-text" for="instructions">Instructions *</label><textarea
           id="instructions"
           name="instructions"
           rows="4"
           cols="50"
           v-model="instructions"
           required
-        ></textarea>
+        ></textarea></div></form> -->
+    <form style="color: black;" class="container">
+
+      <header class="row">
+        <h1 class="title">Add Recipe</h1>
+      </header>
+
+      <div class="row boxes">
+        <div class="form-group">
+          <label for="title">Title *</label>
+          <input type="text" class="form-control" id="title" placeholder="Title" required>
+        </div>
       </div>
 
-      <button type="button" @click="postRecipe" id="button-continue">
-        ADD RECIPE to db
-      </button>
+      <div class="row boxes">
+        <div class="form-group">
+          <label for="serves">Serves</label>
+          <input type="number" class="form-control" name="serves" placeholder="Serves" v-model="serves" required />
+        </div>
+      </div>
+
+      <div class="row boxes">
+        <div class="form-group">
+          <label for="cars">Choose your ingredients:</label>
+          <select class="form-control" placeholder="Ingredients" name="ingredients" v-model="ingredient">
+            <option v-for="ingredient in ingredients" :key="ingredient.id">
+              {{ ingredient.name }}
+            </option>
+          </select>
+          <input style="margin-top: 1%" class="form-control" type="text" name="amount" placeholder="Amount" v-model="amount" required />
+          <button class="btn btn-success" type="button" @click="addMeasurement">Add</button>
+        </div>
+      </div>
+
+      <div class="row boxes">
+        <div class="row" ref="selectedMeasurements">
+          <h2></h2>
+          <div>
+            <div class="form-control" v-for="measurement in selectedMeasurements" :key="measurement">
+              {{ measurement.ingredient }}
+              {{ measurement.amount }}
+              <button class="btn btn-danger" @click="deleteMeasurements(measurement)">Delete</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row boxes">
+        <div class="form-group">
+          <label for="exampleFormControlFile1">Image</label>
+          <input type="file" class="form-control-file" id="Import Image">
+        </div>
+      </div>
+
+      <div class="row boxes">
+        <div class="form-group">
+          <label for="exampleFormControlTextarea1">Instructions *</label>
+          <textarea class="form-control" id="instructions" rows="3"></textarea>
+        </div>
+      </div>
+
+      <div class="row boxes">
+        <button type="button" @click="postRecipe" class="btn btn-success" style="color: white">ADD RECIPE</button>
+      </div>
+
     </form>
   </body>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      // TODO fetch ingredients from pinata store
-      ingredients: [
-        { id: 1, name: "one11" },
-        { id: 2, name: "deadad" },
-        { id: 3, name: "dadawd" },
-        { id: 4, name: "yep" },
-        { id: 4, name: "other" },
-      ],
-      ingredient: "",
-      amount: "",
-      serves: "",
-      title: "",
-      instructions: "",
-      selectedMeasurements: [],
-    };
-  },
-
-  methods: {
-    addMeasurement() {
-      if (!this.ingredient || !this.amount) {
-        return;
-      }
-      this.selectedMeasurements.push({
-        ingredient: this.ingredient,
-        amount: this.amount,
-      });
+  export default {
+    data() {
+      return {
+        // TODO fetch ingredients from pinata store
+        ingredients: [{
+          id: 1,
+          name: "Butter"
+        }, {
+          id: 2,
+          name: "Nuts"
+        }, {
+          id: 3,
+          name: "Beans"
+        }, {
+          id: 4,
+          name: "Balls"
+        }, {
+          id: 4,
+          name: "Poop"
+        }, ],
+        ingredient: "",
+        amount: "",
+        serves: "",
+        title: "",
+        instructions: "",
+        selectedMeasurements: [],
+      };
     },
-    deleteMeasurements(n) {
-      this.selectedMeasurements = this.selectedMeasurements.filter(
-        (m) => m.ingredient != n.ingredient || m.amount != n.amount
-      );
+    methods: {
+      addMeasurement() {
+        if (!this.ingredient || !this.amount) {
+          return;
+        }
+        this.selectedMeasurements.push({
+          ingredient: this.ingredient,
+          amount: this.amount,
+        });
+      },
+      deleteMeasurements(n) {
+        this.selectedMeasurements = this.selectedMeasurements.filter(
+          (m) => m.ingredient != n.ingredient || m.amount != n.amount);
+      },
+      postRecipe() {
+        console.log("POST");
+        console.log("measurements", this.selectedMeasurements);
+        console.log("title", this.title);
+        console.log("serves", this.serves);
+        console.log("instructions", this.instructions);
+      },
     },
-    postRecipe() {
-      console.log("POST");
-      console.log("measurements", this.selectedMeasurements);
-      console.log("title", this.title);
-      console.log("serves", this.serves);
-      console.log("instructions", this.instructions);
-    },
-  },
-};
+  };
 </script>
 
 <style>
-* {
-  font-family: "arial";
-  background-color: #ffffff;
-  width: 100%;
-}
+  * {
+    font-family: "arial";
+    background-color: #ffffff;
+  }
 
-/* .input {
-  background-color: #FFFFFF;
-  margin-bottom: 24px;
-  width: 350px;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  align-self: stretch;
-} */
+  body {
+    background-color: #ffffff;
+  }
 
-.left-text {
-  width: 350px;
-  font-size: 14px;
-  font-weight: 600;
-  line-height: normal;
-  color: #191d23;
-  flex: 1;
-  margin-bottom: 4px;
-}
+  .title{
+    margin-top: 2%;
+  }
 
-.bar {
-  border-radius: 4px;
-  padding: 7px 11px;
-  display: flex;
-  align-items: center;
-  align-self: stretch;
-  border: 1px solid #000000;
-}
+  h1 {
+    font-weight: 700;
+  }
 
-.title {
-  font-family: "arial";
-  font-size: 36px;
-  font-weight: 700;
-  line-height: 44px;
-  color: #000000;
-  margin-bottom: 4px;
-  letter-spacing: -0.72px;
-}
+  .boxes{
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
 
-body {
-  background-color: #ffffff;
-  padding: 24px 24px 350px;
-  /* display: flex; */
-  flex-direction: column;
-  align-items: center;
-}
-
-.logo {
-  width: 175px;
-  height: 110px;
-  margin-bottom: 24px;
-  margin-left: auto;
-  margin-right: auto;
-}
-
-header {
-  margin-bottom: 24px;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  width: 350px;
-}
-
-h1 {
-  font-weight: 700;
-  line-height: normal;
-  color: #191d23;
-  align-self: stretch;
-  margin-bottom: 8px;
-}
-
-.subtitle {
-  font-size: 14px;
-  font-weight: 400;
-  line-height: normal;
-  color: #64748b;
-  align-self: stretch;
-}
-
-#button-continue {
-  background-color: #329e63;
-  border-radius: 4px;
-  padding: 6px 12px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 291px;
-  font-size: 16px;
-  font-weight: 600;
-  line-height: normal;
-  color: #ffffff;
-  text-align: center;
-  transition-duration: 0.4s;
-}
-
-#button-continue:hover {
-  box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24),
-    0 17px 50px 0 rgba(0, 0, 0, 0.19);
-}
-
-span {
-  display: block;
-  margin-left: 20px;
-  color: red;
-  font-style: italic;
-}
+  .btn{
+    margin-top: 10px;
+  }
 </style>
