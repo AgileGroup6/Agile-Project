70117717c644d4b199bcf7b064a557cd8441f451
Jaibeer  S Dugal <jaibeer72@gmail.com>
2023-01-26 12:29:37 +0000

    Fixing homepage conflict

--- client/app/src/router/index.js
+++ client/app/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomePage from "../views/HomePage.vue"
 
 const router = createRouter({
 
