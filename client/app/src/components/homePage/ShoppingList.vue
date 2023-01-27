fddcb54aa16eede35347e4a692416f8f7463e594
David Topping <2412905@dundee.ac.uk>
2023-01-27 11:53:24 +0000

    added shopping list

---
+++ client/app/src/components/homePage/ShoppingList.vue
@@ -0,0 +1,49 @@
+<template>
+
+    <div class="container py-4 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+
+            <div class="col col-lg-8 col-xl-6">
+                <div class="card rounded-3">
+                    <div class="card-body p-4">
+
+                        <p class="mb-2"><span class="h2 me-2">Shopping List</span> <span
+                                class="badge bg-danger">Incomplete</span></p>
+
+
+                        <ul class="list-group rounded-0">
+
+                            <li class="list-group-item border-0 d-flex align-items-center ps-0">
+                                <input class="form-check-input me-3" type="checkbox" value="" aria-label="..." />
+                                Garlic
+                            </li>
+
+                            <li class="list-group-item border-0 d-flex align-items-center ps-0">
+                                <input class="form-check-input me-3" type="checkbox" value="" aria-label="..." />
+                                Bicarbonate of soda
+                            </li>
+
+                            <li class="list-group-item border-0 d-flex align-items-center ps-0">
+                                <input class="form-check-input me-3" type="checkbox" value="" aria-label="..." />
+                                Desiccated coconut
+                            </li>
+                        </ul>
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
--- client/app/src/views/HomePage.vue
+++ client/app/src/views/HomePage.vue
@@ -1,32 +1,39 @@
 <template>
   <div class="container">
 
-      
 
 
-      <div class="row">
-          <div class="col mt-2">
-              <IngredientSearch />
-              <ListItem :ingredients="shoppingList"/>
-          </div>
-        </div>
-
-      
-      <div class="row mt-3">
-          <div class="col mt-2">
-            <RouterLink to="/browse">
-            <ButtonComponent label="Browse All" :doOnClick="testFunction"/>     
-            </RouterLink>
-          </div>
+    <!-- search ingredients -->
+    <div class="row">
+      <div class="col mt-2">
+        <IngredientSearch />
+        <!-- <ListItem :ingredients="shoppingList"/> -->
       </div>
+    </div>
 
-      <div class="row mt-3">
-        <div class="col">
-          <ButtonComponent  label="Featured Recipes" :doOnClick="testFunction"/>
-        </div>
+    <!-- shopping list -->
+    <div class="row">
+      <div class="col">
+        <ShoppingList />
       </div>
-      
-     
+    </div>
+
+    <!-- large buttons -->
+    <div class="row mt-3">
+      <div class="col mt-2">
+        <RouterLink to="/browse">
+          <ButtonComponent label="Browse All" :doOnClick="testFunction" />
+        </RouterLink>
+      </div>
+    </div>
+
+    <div class="row mt-3 mb-3">
+      <div class="col">
+        <ButtonComponent label="Featured Recipes" :doOnClick="testFunction" />
+      </div>
+    </div>
+
+
 
   </div>
 </template>
@@ -36,24 +43,25 @@
 export default {
 
   data() {
-        return {
-            shoppingList : [{ ing: 'Curry Powder' }, { ing: 'Pepper' }],
-        }
-    
-},
+    return {
+      shoppingList: [{ ing: 'Curry Powder' }, { ing: 'Pepper' }],
+    }
 
-methods: {
-  testFunction: function () {
-    console.log('test clicked')
+  },
+
+  methods: {
+    testFunction: function () {
+      console.log('test clicked')
+    }
   }
-}
 }
 </script>
 
 <script setup>
-import IngredientSearch from "../components/homePage/IngredientSearch.vue";
+import IngredientSearch from "@/components/homePage/IngredientSearch.vue";
 import ButtonComponent from "@/components/homePage/ButtonComponent.vue";
-import ListItem from "../components/homePage/ListItem.vue";
+// import ListItem from "@/components/homePage/ListItem.vue";
+import ShoppingList from "@/components/homePage/ShoppingList.vue";
 </script>
 
 <style scoped>
