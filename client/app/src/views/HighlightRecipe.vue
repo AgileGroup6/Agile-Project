a1ac971964178727653ead48d3d883ed0409fd1c
kekizl <tophack@outlook.com>
2023-01-30 14:36:11 +0000

    minimum working version for scheduling recipes

--- client/app/src/views/HighlightRecipe.vue
+++ client/app/src/views/HighlightRecipe.vue
@@ -1,111 +1,114 @@
 <template>
-    <body>
-      <head>
-        <title>Highlight Recipe</title>
-      </head>
-   
-      <div>
-  
-        <div class ="container d-flex justify-content-center align-item-center" >
+  <div>
+    <div class ="container d-flex justify-content-center align-item-center" >
     <div class="Highlight">
       <h1>Highlight Recipe</h1>
     </div>
-</div>
-
-        <div class="dropdown-center">
-      <div class = "row" >
-    <button class="btn btn-success dropdown-toggle" type="button" id="categoryButton" data-bs-toggle="dropdown" aria-expanded="false">
-     browse recipes
-    </button>
-    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-      <!-- THIS IS CURRENTLY HARD-CODED DATA!!! CHANGE BEFORE FINAL -->
-      <li><a class="dropdown-item text-center" href="#" @click="select('pasta...')">pasta</a></li>
-      <li><a class="dropdown-item text-center" href="#" @click="select('pankaces')">pankaces</a></li>
-      <li><a class="dropdown-item text-center" href="#" @click="select('salad')">salad</a></li>
-    </ul>
   </div>
-</div>
-       
-<div class = "container overflow-auto mt-4" id = "cardContainer">
-  <div class = "row" id = "row-cards">
 
-    <div class = "col" id = "col-center mt-6">
-      <!--The array is sliced in two so that the items are not added to just one column and are instead added evenly across both of them-->
-      <card v-for="(item, index) in testNames.slice(testNames.length/2)"
-          :key="item.id" 
-          :ingredientName="item.ingredientName"
-          :image="item.image"
-      /> 
+  <div class="dropdown-center">
+    <div class = "row" >
+      <button class="btn btn-success dropdown-toggle mt-4" type="button" id="categoryButton" data-bs-toggle="dropdown" aria-expanded="false">
+        Browse Recipes
+      </button>
+      <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <!-- THIS IS CURRENTLY HARD-CODED DATA!!! CHANGE BEFORE FINAL -->
+        <li v-for="recipe in recipes" :key="recipe.id">
+          <a class="dropdown-item text-center" href="#" @click="select(recipe)">{{recipe.name}}</a>
+        </li>
+      </ul>
     </div>
 
- 
+    <div v-if="selectedRecipe">
+      <div class = "container" id = 'ingredient_card'>
+    
+    <div class = "row text-center mt-4"> 
+        <h3> {{selectedRecipe.name}} </h3>
+    </div>
+    
+    <div class = "row justify-content-center"> 
+        <!-- <img src={{image}}> -->
+        <!-- HARD CODED IMAGE FOR TESTING! CHANGE BEFORE FINAL -->
+        <img src = "../../src/assets/bred1test.jpg"> 
+    </div>
+    <div class = "row justify-content-center mt-4 align-item-center d-flex" > 
+        
+            <div class="col-5 justify-content-center d-flex">
+            <input type="date" v-model="scheduleStart"> 
+            </div>
+            <div class="col-2 justify-content-center d-flex">
+            to
+            </div>
+            <div class="col-5 justify-content-center d-flex">
+              <input type="date" v-model="scheduleEnd">
+            </div>
+        
+    </div>
+    <div class = "row justify-content-center d-flex mt-4">
+        <button class = "btn btn-success " @click="highlightRecipe" >Schedule</button>
+    </div>
+</div>
+
+    
+    </div>
 
   </div>
 </div>
-</div>
-
-    </body>
-  </template>
+</template>
   
-  <script>
+<script>
+import axios from 'axios'
 
-import card from '../components/browseAll/ingredientCard.vue';
+export default {
+  data() {
+    return {
+      selectedRecipe: "",
 
-
-    export default {
-
-        components: {
-        card,
+      recipes: [
+      {
+        id: 1,
+        name: "watered down bread"
       },
-      data() {
-        return {
-         
-        range: 0,
-        testNames: [ {
-          id: 1,
-          ingredientName: 'Baguette',
-          image: '../../public/favicon.ico' //ADD IMAGE - CURRENTLY HARDCODED IN ingredientCard FILE
-        },
-        {
-          id: 2,
-          ingredientName: 'Sourdough',
-          image: '"../views/favicon.ico"' //ADD IMAGE
-        },
-        {
-          id: 3,
-          ingredientName: 'Bagels',
-          image: '../../public/favicon.ico' //ADD IMAGE
-        }]
-      
-        };
+      {
+        id: 2,
+        name: "ice"
       },
-      methods: {
-        select (category) {
-        this.active = !this.active
-        document.getElementById('categoryButton').innerHTML = category;
-
-      //The below is text data to test the formatting of the table.
-      //When connecting the backend, replace with API Call to get ingredients by category.
-      //Set resultsArray from query to testNames (rename for final) to run
-
-      //This clears the array so that when a new category is searched the old items are removed from the DOM
-        this.testNames = [];
-
-        //Pushing items to an array will allow them to be added to the DOM after the component has alrady rendered.
-        //Documentation here: https://vuejs.org/guide/essentials/list.html
-       
-        this.testNames.push({
-          id: 4,
-          ingredientName: "Toast",
-          image: '../../../src/assets/bred1test.jpg' //ADD IMAGE
-        })
-      
-    
-      }
-    
-        
-      },
+      ],
     };
+  },
+  methods: {
+    select(recipe) {
+      this.selectedRecipe = recipe
+    },
+    highlightRecipe(){
+      // this.scheduleStart
+      const id = this.selectedRecipe.id
+      const scheduleStart = this.scheduleStart
+      const scheduleEnd = this.scheduleEnd
+
+      console.log("post")
+      console.log(id);
+      // if null return
+      console.log(scheduleStart);
+      console.log(scheduleEnd);
+
+      const json = {
+        id: this.selectedRecipe.id,
+        scheduleStart: this.scheduleStart,
+        scheduleEnd: this.scheduleEnd
+      }
+
+      axios.post("https://cloud.caydey.com/api/setFeatured", json).then(() =>{
+        alert("schedules")
+      }).catch((err) => {
+        console.log(err);
+        alert("error scheduling message")
+      })
+      
+
+    }
+  },
+};
 </script>
 
 
