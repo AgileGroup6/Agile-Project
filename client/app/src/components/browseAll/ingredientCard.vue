75a51e30b101cb56c699bebbcc68a162b1372460
kekizl <tophack@outlook.com>
2023-01-31 11:34:57 +0000

    tests2

--- client/app/src/components/browseAll/ingredientCard.vue
+++ client/app/src/components/browseAll/ingredientCard.vue
@@ -2,26 +2,34 @@
     <div class="card" id="ingredient_card" style="width: 18rem;">
         <img class="card-img-top" src="../../../src/assets/bred1test.jpg" alt="Card image cap">
         <div class="card-body">
-            <h5 class="card-title"> {{ingredientName}} </h5>
-            <button class = "btn btn-success" @click="addToCart()">Add to cart</button>
+            <h5 class="card-title"> {{ ingredientName }} </h5>
+            <button class="btn btn-success" @click="addToCart()">Add to Shopping List</button>
         </div>
     </div>
 </template>
 
 
-<script> 
+<script>
+import { useIngridentsStore } from "../../stores/ingridentsStore.js";
 
-function addToCart(){
-    
+const store = useIngridentsStore();
+
+function addToCart() {
+    if (!item) return;
+    store.addItem(item);
+    console.log(store.shoppingList);
 }
+
+
+
 
 export default {
     //Uses view properties to fill the cards with their relevant fields
-    props: ['ingredientName','image'],
-    
+    props: ['ingredientName', 'image'],
+
     methods: {
-        addToCart(){
-       
+        addToCart() {
+
         }
     }
 
