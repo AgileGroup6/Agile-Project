38b03a93d392275760f8a77ad70936c644c86852
kekizl <tophack@outlook.com>
2023-01-31 13:56:57 +0000

    Revert "fixed issue from merge request"
    This reverts commit 0d0d5e7b953aa19f5af968e61f6f776f4feabf51.

--- client/app/src/stores/ingridentsStore.js
+++ client/app/src/stores/ingridentsStore.js
@@ -4,37 +4,36 @@ import { useStorage } from '@vueuse/core'
 
 
 export const useIngridentsStore = defineStore({
-  id: "ingredients",
-  state: () => ({
-    /** @type {string[]} */
-    items: [],
-    shoppingList: useStorage('shoppingList', [])
-  }),
-  getters: {
-    getItems: () => {
-      return this.items;
-    }
-
-
-  },
-  actions: {
-    addItem(item) {
-      const existingItem = this.shoppingList.find((it) => it.name === item.name);
-      if (!existingItem)
-        this.shoppingList.push(item);
-      // doube up items is needed
-      else
-        return;
+    id: "ingredients",
+    state: () => ({
+      /** @type {string[]} */
+      items : [],
+      shoppingList: useStorage('shoppingList', [])
+    }),
+    getters: {
+      getItems:() => {
+        return this.items;
+      }
+        
     },
+    actions: {
+      addItem(item) {
+        const existingItem = this.shoppingList.find((it) => it.name === item.name);
+        if(!existingItem)
+            this.shoppingList.push(item);
+        // doube up items is needed
+        else
+            return; 
+      },
 
-    async updateAllIngredients() {
-      try {
-        const res = await axios.get("https://lgl.caydey.com/api/allIngredients");
-        this.items = res.data.data;
-      }
-      catch (ex) {
-        console.log(ex);
+      async updateAllIngredients(){
+        try {
+            const res = await axios.get("https://lgl.caydey.com/api/allIngredients");
+            this.items = res.data.data;
+        }
+        catch(ex){
+            console.log(ex); 
+        }
       }
     }
-  }
-});
+  });
