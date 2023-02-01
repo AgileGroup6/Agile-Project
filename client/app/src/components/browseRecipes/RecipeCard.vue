048e702bc73d120c10eef8c653fc57c919aff22c
kekizl <tophack@outlook.com>
2023-02-01 14:06:29 +0000

    added cards and improved scalability

---
+++ client/app/src/components/browseRecipes/RecipeCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="card" id="ingredient_card">
+        <img class="card-img-top" src="@/assets/recipe1.jpg" alt="Card image cap">
+        <div class="card-body">
+            <h5 class="card-title"> {{ curRecipe.name }} </h5>
+            <button class="btn btn-success">View Recipe</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['curRecipe'],
+}
+
+</script>
+
