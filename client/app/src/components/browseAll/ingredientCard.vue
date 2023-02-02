cacdddcf3814adc81d3af14fe7713b0b25f86591
calebh210 <calebh210@github.com>
2023-02-02 09:52:32 -0500

    added to list message

--- client/app/src/components/browseAll/ingredientCard.vue
+++ client/app/src/components/browseAll/ingredientCard.vue
@@ -3,8 +3,11 @@
         <img class="card-img-top" src="../../../src/assets/bred1test.jpg" alt="Card image cap">
         <div class="card-body">
             <h5 class="card-title"> {{ ingredientArray.name }} </h5>
-            <button class="btn btn-success" @click="addToCart(ingredientArray)">
+            <!-- Because we have no id var in the json for ingredients, I use the name as the ID instead to change the button to added-->
+            <button class="btn btn-success" v-bind:id="ingredientArray.name" @click="addToCart(ingredientArray);">
+                <div>
                 Add to Shopping List
+                </div>
             </button>
         </div>
     </div>
@@ -20,10 +23,14 @@ export default {
     //Uses view properties to fill the cards with their relevant fields
 
     props: ['ingredientArray'],
-
+    data() {
+        //Cannot access props directly from a method, must be assigned to a variable under data first
+        return { buttonID: this.ingredientArray.name }
+    },
     methods: {
         addToCart(item) {
             store.shoppingList.push(item);
+            document.getElementById(this.buttonID).innerHTML = "Added!"
 
             //console.log(store.categories);
 
