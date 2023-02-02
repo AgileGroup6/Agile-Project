f410befc6c46a07f2d54fae7ed6606b233b26e05
FinnHagan <2410686@dundee.ac.uk>
2023-02-02 15:38:49 +0000

    Initial commit

---
+++ client/app/src/components/remove/RemoveCard.vue
@@ -0,0 +1,21 @@
+
+<!-- Recipe Card for recipes displayed in the remove recipes page-->
+<template>
+    <!-- Uses styling as present for browse all ingredients-->
+    <div class="card" id="ingredient_card">
+        <img class="card-img-top" src="@/assets/recipe1.jpg" alt="Card image cap">
+        <div class="card-body">
+            <!--card to remove a recipe-->
+            <h5 class="card-title"> {{ curRecipe.name }} </h5>
+            <button class="btn btn-danger" @click="removeRecipe({ recipe_id: curRecipe.id })">Remove Recipe</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['curRecipe'],
+}
+
+</script>
+
--- client/app/src/views/RemoveRecipe.vue
+++ client/app/src/views/RemoveRecipe.vue
@@ -1,8 +1,36 @@
 <template>
-  <div class="jumbotron jumbotron-fluid">
+  <div class="jumbotron jumbotron-fluid text-center">
     <div class="container">
-      <h1 class="display-6 text-center">Remove Recipe</h1>
-      <p class="lead text-center">Please select the recipe you would like to remvove from the recipe list.</p>
+      <h1 class="display-6 mb-3">Remove a recipe</h1>
+      <!-- Uses styling as present for browse all ingredients-->
+      <div class="ingredientsGrid">
+        <RemoveCard v-for="recipe in recStore.items" class="card" :key="recipe.id" :curRecipe="recipe" />
+      </div>
     </div>
   </div>
 </template>
+<script setup>
+import { useRecipestore } from "../stores/RecipeStore"
+import RemoveCard from '../components/remove/RemoveCard.vue';
+</script>
+
+<script>
+const recStore = useRecipestore();
+export default {
+  components: {
+    RemoveCard,
+  }
+}
+</script>
+
+<style scoped>
+.card {
+  display: inline-block;
+  margin-right: 8px;
+  margin-bottom: 8px;
+}
+
+.ingredientsGrid {
+  margin-top: 20px;
+}
+</style>
